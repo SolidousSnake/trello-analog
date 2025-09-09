e5d3b08c9e802e06973b51ce5a5dde43b538cd36
Timour <67632745+SolidousSnake@users.noreply.github.com>
2025-09-09 21:50:55 +0300

    refactor(Implemented HashRouter)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import HomePage from "./pages/Home";
 import BoardPage from "./pages/Board";
 import NotFoundPage from "./pages/NotFound";
